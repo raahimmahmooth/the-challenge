921b6ed386a22b705af52242f4fcfe2e7f5f3528
raahim_mahMOOth <raahimmahmooth@gmail.com>
2025-04-14 19:46:13 +0530

    Fix: Bind Flask app to dynamic port for Railway

--- app.py
+++ app.py
@@ -39,7 +39,5 @@ def login():
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
