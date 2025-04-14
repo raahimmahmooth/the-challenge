82ac314f30c01d469c54fbfae7c6ee98be341c09
raahim_mahMOOth <raahimmahmooth@gmail.com>
2025-04-14 19:16:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Dummy user database
+users = {
+    'antonio': {
+        'password': 'admin123',#weakpassword
+        'flag': 'THM{weak_authentication_flag}'
+    },
+    'user1': {
+        'password': 'password',
+        'flag': ''
+    },
+    'raahim':{
+        'password':11200322,
+        'flag':'THM{try_to_find_me}'
+    }
+
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users:
+            if users[username]['password'] == password:
+                flag = users[username]['flag']
+                return f"<h2>Welcome, {username}!</h2><p>Your flag: <strong>{flag}</strong></p>"
+            else:
+                error = 'Incorrect password.'
+        else:
+            error = 'Username not found.'
+
+    return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
